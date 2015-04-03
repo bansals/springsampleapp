cc9a33d3b33e77b385c674156477b8d48f4f02de
Sachin Bansal <sbansal@expedia.com>
2015-04-02 17:08:27 -0700

    adding initial project setup

---
+++ src/main/java/com/sachin/sampleapp/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.sachin.sampleapp;
+
+/**
+ * Created by sbansal on 4/2/15.
+ */
+public class CalculatorService
+{
+    public int multiple(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/tests/java/com/sachin/sampleapp/CalculatorServiceTest.java
@@ -0,0 +1,9 @@
+package com.sachin.sampleapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorServiceTest {
+
+}
